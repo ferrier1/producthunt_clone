844988c1c918a952c2be6e67596a0a3151ebe3ba
johnnydrama92 <james.ferrier01@gmail.com>
2018-04-21 18:01:41 +0100

    static media files and create page

--- products/views.py
+++ products/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from .models import Product
+
 
 def home(request):
     return render(request, 'products/home.html')
 
 @login_required
 def create(request):
-    return render(request, 'products/create.html')
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['text'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
+            product = Product()
+            product.title = request.POST['title']
+            product.text = request.POST['text']
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url']
+            product.icon = request.FILES['icon']
+            product.image = request.FILES['image']
+            product.date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save()
+            return redirect('home')
+    else:
+        return render(request, 'products/create.html')
