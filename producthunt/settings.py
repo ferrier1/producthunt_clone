c5d8cea1c66ad9506173add4cf034f3cf88b4e1a
James Ferrier <james.ferrier01@gmail.com>
2018-04-23 19:31:02 +0100

    Update settings.py

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'producthuntdb',
         'USER': 'postgres',
-        'PASSWORD': 'Comput1ng6^',
+        'PASSWORD': 'xxxx',
         'HOST': 'localhost',
         'PORT': '5432',
 
